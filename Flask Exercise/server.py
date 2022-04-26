ec92cb5bae579685001e64a104ce01c2532bc997
karolkecinski <58360925+karolkecinski@users.noreply.github.com>
2022-04-26 14:03:05 +0200

    Flask Exercise completed

--- Flask Exercise/server.py
+++ Flask Exercise/server.py
@@ -1,7 +1,9 @@
+from werkzeug.utils import secure_filename
 from flask import Flask, render_template, request
 import os
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+app.config['UPLOAD_FOLDER'] = 'static'
 
 '''
 Tutorial help: https://www.tutorialspoint.com/flask/flask_file_uploading.htm
@@ -13,7 +15,12 @@ def start():
 	
 @app.route("/selected_image", methods=['POST'])
 def select_query_image():
-    pass
+    if request.method == 'POST':
+       file = request.files['file']
+       filename = secure_filename(file.filename)
+       pathToFile = os.path.join('static', filename)
+       file.save(pathToFile)
+       return render_template('show_image.html', image = filename)
 		
 if __name__ == '__main__':
    app.run(debug = True)
