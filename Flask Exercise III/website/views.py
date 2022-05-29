4db861f633264dbb7c3b4ae0d664fa93a8f9df37
karolkecinski <58360925+karolkecinski@users.noreply.github.com>
2022-05-29 20:59:31 +0200

    Update 29.05.2022

--- Flask Exercise III/website/views.py
+++ Flask Exercise III/website/views.py
@@ -2,24 +2,25 @@ from asyncio import selector_events
 from flask import request, flash, redirect, Blueprint, render_template
 import os
 from werkzeug.utils import secure_filename
-from query_handler import Handler
+from search_engine.handler import Handler
 
 views = Blueprint('views', __name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-QUERY_FOLDER = f'static{os.sep}queries'
+QUERY_FOLDER = f'static{os.sep}uploaded'
 PACKAGE = 'website'
 ##DATA_FOLDER = f'search_engine{os.sep}data{os.sep}images{os.sep}training'
 
 ALLOWED_EXT = ['jpg', 'png']
 handler = Handler()
+selected_image = ""
 
 @views.route("/")
 def index():
     global selected_image
     return render_template("start.html", selected_image=selected_image)# selected_image)
 
-@views.route("/selected_image", methods=['POST, GET'])
+@views.route("/selected_image", methods=['POST', 'GET'])
 def select_query_image():
 
     if request.method == 'GET':
@@ -38,7 +39,7 @@ def select_query_image():
             flash(f'No file chosen', 'error')
             return redirect('/')
 
-        filename, ext = secure_filename(file.filename.lower().split('.'))
+        filename, ext = secure_filename(file.filename.lower()).split('.')
         if ext not in ALLOWED_EXT:
             flash(f'Invalid filetype: .{ext}', 'error')
             return redirect('/')
